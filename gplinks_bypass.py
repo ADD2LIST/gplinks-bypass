f29b14218c43b4b84c7dd8a054a9b23f16a22e6c
Bibek Pradhan <134042662+ADD2LIST@users.noreply.github.com>
2023-05-28 12:39:25 +0530

    Update gplinks_bypass.py

--- gplinks_bypass.py
+++ gplinks_bypass.py
@@ -1,33 +1,65 @@
-
-
 import time
+
 import cloudscraper
+
 from bs4 import BeautifulSoup
 
-import requests
-# eg: https://gplinks.co/XXXX
-url = "https://gplinks.co/Z94r6"
+import streamlit as st
 
 # =======================================
 
 def gplinks_bypass(url: str):
- client = cloudscraper.create_scraper(allow_brotli=False)  
- domain ="https://gplinks.co/"
- referer = "https://mynewsmedia.co/"
 
- vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
- url = f"{url}/?{vid}"
+    client = cloudscraper.create_scraper(allow_brotli=False)
 
- response = client.get(url, allow_redirects=False)
- soup = BeautifulSoup(response.content, "html.parser")
-    
-    
- inputs = soup.find(id="go-link").find_all(name="input")
- data = { input.get('name'): input.get('value') for input in inputs }
-    
- time.sleep(10)
- headers={"x-requested-with": "XMLHttpRequest"}
- bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
- return bypassed_url
+    domain = "https://gplinks.co/"
 
-print(gplinks_bypass(url))
+    referer = "https://mynewsmedia.co/"
+
+    vid = client.get(url, allow_redirects=False).headers["Location"].split("=")[-1]
+
+    url = f"{url}/?{vid}"
+
+    response = client.get(url, allow_redirects=False)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    inputs = soup.find(id="go-link").find_all(name="input")
+
+    data = {input.get('name'): input.get('value') for input in inputs}
+
+    time.sleep(10)
+
+    headers = {"x-requested-with": "XMLHttpRequest"}
+
+    bypassed_url = client.post(domain + "links/go", data=data, headers=headers).json()["url"]
+
+    return bypassed_url
+
+# Streamlit app
+
+st.title("GPLinks Bypass")
+
+# Input URL
+
+url = st.text_input("Enter GPLinks URL")
+
+# Bypass button
+
+if st.button("Bypass"):
+
+    if url:
+
+        bypassed_url = gplinks_bypass(url)
+
+        st.success(f"Bypassed URL: {bypassed_url}")
+
+    else:
+
+        st.warning("Please enter a GPLinks URL.")
+
+
+
+
+ 
+ 
